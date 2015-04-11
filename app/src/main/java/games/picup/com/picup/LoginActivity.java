b9659b50b08c1d503bf6923ef6aa748eee2011e7
Freddie <fv4.programmer@gmail.com>
2015-04-11 14:26:49 -0400

    Refactor/Fix fragment code & layouts

--- app/src/main/java/games/picup/com/picup/LoginActivity.java
+++ app/src/main/java/games/picup/com/picup/LoginActivity.java
@@ -1,31 +1,73 @@
 package games.picup.com.picup;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 
 public class LoginActivity extends FragmentActivity {
 
+    private static Button mSignUpButton;
+    private static Button confirm;
+    private SignUpFragment sign;
+    private static UserSignUpInfoFragment info;
+
+
+    //Give your SharedPreferences file a name and save it to a static variable
+    public static final String PREFS_NAME = "MyPrefsFile";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new LoginFragment())
-                    .commit();
-        }
+        setContentView(R.layout.activity_main);
+
+        // Enable Local Datastore.
+//        Parse.enableLocalDatastore(this);
+
+//        SharedPreferences settings = getSharedPreferences(LoginActivity.PREFS_NAME, 0);
+//        //Get "hasLoggedIn" value. If the value doesn't exist yet false is returned
+//        boolean hasLoggedIn = settings.getBoolean("hasLoggedIn", false);
+//
+//        final Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+//
+//        if (hasLoggedIn) {
+//            startActivity(intent);
+//            LoginActivity.this.finish();
+//        }
+
+        sign = new SignUpFragment();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction trans = fm.beginTransaction();
+        trans.add(R.id.main_container, sign, "User Sign-Up Fragment");
+        trans.commit();
+
+//
+//        // User has successfully logged in, save this information
+//        // We need an Editor object to make preference changes.
+//        SharedPreferences sets = getSharedPreferences(LoginActivity.PREFS_NAME, 0); // 0 - for private mode
+//        SharedPreferences.Editor editor = sets.edit();
+//        //Set "hasLoggedIn" to true
+//        editor.putBoolean("hasLoggedIn", true);
+//        // Commit the edits!
+//        editor.commit();
+//        startActivity(intent);
+//        LoginActivity.this.finish();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 
@@ -33,6 +75,7 @@ public class LoginActivity extends FragmentActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
+
         return true;
     }
 
@@ -51,38 +94,59 @@ public class LoginActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-
-    static SignUpFragment signup;
-    public static class LoginFragment extends Fragment {
-
-        public LoginFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-
-            FragmentManager fm = getFragmentManager();
-            FragmentTransaction trans = fm.beginTransaction();
-            trans.add(R.id.login_container, signup, "User Sign-Up Fragment");
-            trans.commit();
-
-
-            return rootView;
-        }
-    }
-
     public static class SignUpFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_signup, container, false);
 
-            return rootView;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_signup, container, false);
+//            Parse.initialize(getActivity(), "yzyZoFIKPhTU9yOrC6ZcNOxoemEhL35ldYQM6UzR", "arrposL9cr9UAem4aGikI2Ts2FYfhGT8jaZ6xLXh");
+
+            mSignUpButton = (Button) view.findViewById(R.id.sign_up_button);
+
+            mSignUpButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    info = new UserSignUpInfoFragment();
+
+                    FragmentManager fm = getFragmentManager();
+                    FragmentTransaction trans = fm.beginTransaction();
+                    trans.replace(R.id.main_container, info, "User Sign-Up Fragment");
+                    trans.commit();
+                }
+            });
+            return view;
         }
     }
+
+    public static class UserSignUpInfoFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_userinfo, container, false);
+
+            confirm = (Button) view.findViewById(R.id.confirm_signup);
+//            final EditText name = (EditText) view.findViewById(R.id.nameText);
+//            final EditText phoneNumber = (EditText) view.findViewById(R.id.phoneNumber);
+//
+            confirm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    String nameString = name.getText().toString();
+//                    String phoneString = phoneNumber.getText().toString();
+//
+//                    ParseObject testObject = new ParseObject("SignUpObject");
+//                    testObject.put("Name", nameString);
+//                    testObject.put("Number", phoneString);
+//                    testObject.saveInBackground();
+//
+//                    Intent i = new Intent(getActivity(), FeedActivity.class);
+//                    startActivity(i);
+                }
+            });
+
+            return view;
+        }
+    }
+
 }
