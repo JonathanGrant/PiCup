afa99f0e055536d637386b093901d9d95095648a
Freddie <fv4.programmer@gmail.com>
2015-04-11 15:03:14 -0400

    Add games list activity layout

--- app/src/main/java/games/picup/com/picup/GameListActivity.java
+++ app/src/main/java/games/picup/com/picup/GameListActivity.java
@@ -1,7 +1,19 @@
 package games.picup.com.picup;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 /**
  * Created by Freddie4 on 4/11/2015.
  */
-public class GameListActivity {
+public class GameListActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_list);
+
+    }
+
 }
