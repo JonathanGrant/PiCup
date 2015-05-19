6037829e088ff133c1e454f71f4ae06760f30a1a
Jonathan Allen Grant <jonathanallengrant@gmail.com>
2015-05-19 14:09:35 -0700

    USC email to log in only

--- app/src/main/java/games/picup/com/picup/LoginActivity.java
+++ app/src/main/java/games/picup/com/picup/LoginActivity.java
@@ -169,43 +169,60 @@ public class LoginActivity extends FragmentActivity {
                     String userString = mUsername.getText().toString();
                     String passString = mPassword.getText().toString();
 
-                    //create Parse User
-                    ParseObject.registerSubclass(ParseUser.class);
-                    ParseUser user = new ParseUser();
-                    user.setUsername(userString);
-                    //user.setEmail() no email functionality yet... needs to be added
-                    user.setPassword(passString);
+                    //Check if email given is a USC Email
+                    //first clear string of all extra spaces - there are no spaces in an email
+                    userString = userString.replaceAll(" ","");
+                    //make lowercase
+                    userString = userString.toLowerCase();
+                    mUsername.setText(userString); //display email w/o spaces now + lowercase
+                    if(userString.endsWith("@usc.edu") && passString.length()>3) //check if it is a usc address + password is at least four characters
+                    {
+                        //create Parse User
+                        ParseObject.registerSubclass(ParseUser.class);
+                        ParseUser user = new ParseUser();
+                        user.setUsername(userString);
+                        //user.setEmail() no email functionality yet... needs to be added
+                        user.setPassword(passString);
 
-                    //create account in database
-                    Parse.initialize(getActivity(), "B4rIuWBWbeVaHrdtdnUZcC5ziI2cqAm1ZneexOXy", "mcGiMCshfXbCH29AXXiiK7lU9KBxrCRb0r00psWB");
-                    user.signUpInBackground(new SignUpCallback() {
-                        public void done(com.parse.ParseException e) {
-                            if (e == null) {
-                                // Hooray! Let them use the app now.
-                                Intent i = new Intent(getActivity(), GameList.class);
-                                startActivity(i);
+                        //create account in database
+                        Parse.initialize(getActivity(), "B4rIuWBWbeVaHrdtdnUZcC5ziI2cqAm1ZneexOXy", "mcGiMCshfXbCH29AXXiiK7lU9KBxrCRb0r00psWB");
+                        user.signUpInBackground(new SignUpCallback() {
+                            public void done(com.parse.ParseException e) {
+                                if (e == null) {
+                                    // Hooray! Let them use the app now.
+                                    Intent i = new Intent(getActivity(), GameList.class);
+                                    startActivity(i);
 
-                                // User has successfully logged in, save this information
-                                // We need an Editor object to make preference changes.
-                                SharedPreferences sets = getActivity().getSharedPreferences(LoginActivity.PREFFS, 0); // 0 - for private mode
-                                SharedPreferences.Editor editor = sets.edit();
-                                sets.edit().clear().commit();
-                                //Set "hasLoggedIn" to true
-                                editor.putBoolean("hasLoggedIn", true);
-                                // Commit the edits!
-                                editor.commit();
-                                startActivity(intent);
-                                getActivity().finish();
+                                    // User has successfully logged in, save this information
+                                    // We need an Editor object to make preference changes.
+                                    SharedPreferences sets = getActivity().getSharedPreferences(LoginActivity.PREFFS, 0); // 0 - for private mode
+                                    SharedPreferences.Editor editor = sets.edit();
+                                    sets.edit().clear().commit();
+                                    //Set "hasLoggedIn" to true
+                                    editor.putBoolean("hasLoggedIn", true);
+                                    // Commit the edits!
+                                    editor.commit();
+                                    startActivity(intent);
+                                    getActivity().finish();
 
-                                toast.setText("Created Account!");
-                                toast.show();
-                            } else {
-                                // Sign up didn't succeed. Look at the ParseException
-                                // to figure out what went wrong
-                                toast.show();
+                                    toast.setText("Created Account!");
+                                    toast.show();
+                                } else {
+                                    // Sign up didn't succeed. Look at the ParseException
+                                    // to figure out what went wrong
+                                    toast.setText("Sign Up Failed");
+                                    toast.show();
+                                }
                             }
-                        }
-                    });
+                        });
+                    } else if(!userString.endsWith("@usc.edu")) {
+                        toast.setText("Only valid USC email addresses are accepted.");
+                        toast.show();
+                    } else {
+                        //password isn't long enough
+                        toast.setText("Your password must be at least four characters long.");
+                        toast.show();
+                    }
                 }
             });
 
